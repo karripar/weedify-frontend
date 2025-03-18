9b6e73a2b95263158a447b24d7b511d9abecde8e
karripartanen <karripartanen25@gmail.com>
2025-03-18 13:35:15 +0200

    update

---
+++ src/utils/colors.ts
@@ -0,0 +1,21 @@
+
+const HexColors = {
+  //Primary colors
+  'dark-green': '#1E4D3A',
+  'light-green': '#4CAF7E',
+
+  //Alternate colors
+  'light-purple': '#C7B7E2',
+
+  //Neutral colors
+  'dark-grey': '#333333',
+  'almost-white': '#F5F5F5',
+  'white': '#FFFFFF',
+  'light-gray': '#E0E0E0',
+};
+
+const HexGradients = {
+  'primary-green': ['#1E4D3A', '#4CAF7E'],
+};
+
+export { HexColors, HexGradients };
